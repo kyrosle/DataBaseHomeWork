799bab79c71230c65fee6925eb67953d44795741
kyrosle <le@90e.com>
2022-09-27 13:58:15 +0800

    ff

--- HomeWork.api/Service/StaffService.cs
+++ HomeWork.api/Service/StaffService.cs
@@ -4,6 +4,8 @@ using HomeWork.Share.Dtos;
 using HomeWork.Share.Parmeters;
 using MapsterMapper;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Runtime.Intrinsics.Arm;
 
 namespace HomeWork.Api.Service
 {
@@ -21,6 +23,12 @@ namespace HomeWork.Api.Service
         {
             try
             {
+                bool isExist = db.Staffs.AsNoTracking().Where(st => st.Name.Trim().Equals(model.Name)).Any();
+                if (isExist)
+                {
+                    return new ApiResponse("Staff is Existed");
+                }
+
                 var staff = mapper.Map<Staff>(model);
                 await db.Staffs.AddAsync(staff);
                 await db.SaveChangesAsync();
@@ -32,23 +40,45 @@ namespace HomeWork.Api.Service
             }
         }
 
-        public Task<ApiResponse> DeleteAsync(int id)
+        public async Task<ApiResponse> DeleteAsync(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var staff = await db.Staffs.SingleAsync(st => st.Id.Equals(id));
+                if (staff is null)
+                {
+                    return new ApiResponse("Id not Found");
+                }
+                else
+                {
+                    db.Staffs.Remove(staff);
+                    var result = await db.SaveChangesAsync();
+                    return new ApiResponse(true, $"Delete {result} Records");
+                }
+            }
+            catch (Exception e)
+            {
+                return new ApiResponse(e.Message);
+            }
         }
 
         public async Task<ApiResponse> GetAllAsync(QueryParameter parameter)
         {
             try
             {
-                //var staffsQry = from staff in db.Staffs select staff;
                 if (parameter.PageSize == 0) parameter.PageSize = 20;
-                IQueryable<Staff> staffsQuery = db.Staffs
-                    .Skip(parameter.PageIndex * parameter.PageSize)
-                    .Take(parameter.PageSize)
-                    .Where(st => string.IsNullOrWhiteSpace(parameter.Search) || st.Name.Contains(parameter.Search));
+                //IQueryable<Staff> staffsQuery = db.Staffs
+                //    .Skip(parameter.PageIndex * parameter.PageSize)
+                //    .Take(parameter.PageSize)
+                //    .Where(st => string.IsNullOrWhiteSpace(parameter.Search) || st.Name.Contains(parameter.Search));
+                IQueryable<Staff> staffsQuery = from st in db.Staffs
+                                                join dp in db.Departments on st.DepartmentId equals dp.Id
+                                                join pt in db.Posts on st.PostId equals pt.Id
+                                                select new StaffDto()
+                                                {
+                                                };
                 var staffModels = await staffsQuery.ToArrayAsync();
-                var staffs = staffModels.Select(stm => mapper.Map<StaffDto>(stm)).ToArray();
+                //var staffs = staffModels.Select(stm => mapper.Map<StaffDto>(stm)).ToArray();
                 return new ApiResponse(true, staffs);
             }
             catch (Exception e)
@@ -57,14 +87,39 @@ namespace HomeWork.Api.Service
             }
         }
 
-        public Task<ApiResponse> GetSingleAsync(int id)
+        public async Task<ApiResponse> GetSingleAsync(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var staff = await db.Staffs.SingleAsync(st => st.Id.Equals(id));
+                if (staff is not null)
+                {
+                    return new ApiResponse(true, staff);
+                }
+                else
+                {
+                    return new ApiResponse("Id not Found");
+                }
+            }
+            catch (Exception e)
+            {
+                return new ApiResponse(e.Message);
+            }
         }
 
-        public Task<ApiResponse> UpdateAsync(StaffDto model)
+        public async Task<ApiResponse> UpdateAsync(StaffDto model)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var staff = mapper.Map<Staff>(model);
+                db.Staffs.Update(staff);
+                var result = await db.SaveChangesAsync();
+                return new ApiResponse(true, $"Updated {result} record(s)");
+            }
+            catch (Exception e)
+            {
+                return new ApiResponse(e.Message);
+            }
         }
     }
 }
